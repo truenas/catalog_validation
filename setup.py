74508b4785e0fbaa249de2704dd6640754c5b7d3
Waqar Ahmed <waqarahmedjoyia@live.com>
2020-11-25 16:00:54 +0500

    Add setup.py for package

---
+++ setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+VERSION = '0.1'
+
+setup(
+    name='catalog_validation',
+    description='Validate TrueNAS Catalog(s)',
+    version=VERSION,
+    include_package_data=True,
+    packages=['catalog_validation'],
+    license='GNU3',
+    platforms='any',
+    entry_points={
+        'console_scripts': [
+            'catalog_validate = scripts.catalog_validate:main',
+        ],
+    },
+)
