6492a2ee6a09b30264a83c9f999a201713ec8cb4
Waqar Ahmed <waqarahmedjoyia@live.com>
2020-11-26 22:18:28 +0500

    Separate k8s python client usage

--- catalog_validation/setup_kubernetes.py
+++ catalog_validation/setup_kubernetes.py
@@ -1,12 +1,8 @@
 import subprocess
 import time
 
-from kubernetes import client, config
-
 from .exceptions import KubernetesSetupException
-
-
-KUBECONFIG_FILE = '/etc/rancher/k3s/k3s.yaml'
+from .k8s.api_client import api_client
 
 
 def setup_kubernetes_cluster():
@@ -25,18 +21,16 @@ def setup_kubernetes_cluster():
         time.sleep(30)
 
     # Now k3s should report itself as running
-    config.load_kube_config(config_file=KUBECONFIG_FILE)
-    api_client = client.api_client.ApiClient()
-    core_api = client.CoreV1Api(api_client)
-    # Let's give it some more time to make sure that the node is not tainted and are actually usable
-    count = 0
-    while True:
-        if count > 4:
-            raise KubernetesSetupException('Failed to configure k3s node')
+    with api_client() as core_api:
+        # Let's give it some more time to make sure that the node is not tainted and are actually usable
+        count = 0
+        while True:
+            if count > 4:
+                raise KubernetesSetupException('Failed to configure k3s node')
 
-        nodes = core_api.list_node().items
-        if not nodes or nodes[0].spec.taints:
-            count += 1
-            time.sleep(30 * count)
-        else:
-            break
+            nodes = core_api.list_node().items
+            if not nodes or nodes[0].spec.taints:
+                count += 1
+                time.sleep(30 * count)
+            else:
+                break
