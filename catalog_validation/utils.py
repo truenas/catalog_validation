e25bdb1d66b7b0c7eec0d03e571b5928e2fd6ebd
Waqar Ahmed <waqarahmedjoyia@live.com>
2020-12-08 04:59:03 +0500

    Correctly check key existence

--- catalog_validation/utils.py
+++ catalog_validation/utils.py
@@ -77,5 +77,5 @@ def validate_key_value_types(data_to_check, mapping, verrors, schema):
 
         if required and key not in data_to_check:
             verrors.add(f'{schema}.{key}', f'Missing required {key!r} key.')
-        elif not isinstance(data_to_check[key], value_type):
+        elif key in data_to_check and not isinstance(data_to_check[key], value_type):
             verrors.add(f'{schema}.{key}', f'{key!r} value should be a {value_type.__name__!r}')
