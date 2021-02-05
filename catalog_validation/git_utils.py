0a566d5a99886044668f4c4b18cefa4352b2eb82
Waqar Ahmed <waqarahmedjoyia@live.com>
2021-02-05 16:33:08 +0500

    Do not check removed versions

--- catalog_validation/git_utils.py
+++ catalog_validation/git_utils.py
@@ -24,6 +24,11 @@ def get_affected_catalog_items_with_versions(catalog_path, base_branch='master')
             continue
 
         train, item, version, file_path_under_version = file_path.split('/', 3)
+
+        if not os.path.exists(os.path.join(catalog_path, train, item, version)):
+            # In case the version was deleted/removed and does not exist, we are not going to try and check that
+            continue
+
         catalog_item = CatalogItem(train=train, item=item, version=version)
         if catalog_item in items_to_be_checked:
             continue
