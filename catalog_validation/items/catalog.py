6fc2992f836d8aef09902dad0a308a7352eab3af
Waqar Ahmed <waqarahmedjoyia@live.com>
2023-02-21 03:48:47 +0500

    Have ix-migrations being exempt from considered a train in a catalog
    (cherry picked from commit b887e36e9ecd2dd1ca90c61ca6ceaa201fcdba06)

--- catalog_validation/items/catalog.py
+++ catalog_validation/items/catalog.py
@@ -25,7 +25,9 @@ def retrieve_train_names(location: str, all_trains=True, trains_filter=None) -> 
         if (
             not (all_trains or train in trains_filter) or not os.path.isdir(
                 os.path.join(location, train)
-            ) or train.startswith('.') or train in ('library', 'docs') or not VALID_TRAIN_REGEX.match(train)
+            ) or train.startswith('.') or train in (
+                'library', 'docs', 'ix-migrations'
+            ) or not VALID_TRAIN_REGEX.match(train)
         ):
             continue
         train_names.append(train)
