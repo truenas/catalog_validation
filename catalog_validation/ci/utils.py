426880297fe070106e75ed25346427d96d7c736a
Waqar Ahmed <waqarahmedjoyia@live.com>
2023-03-13 11:42:16 +0500

    Add method to validate trains in app dev directory

--- catalog_validation/ci/utils.py
+++ catalog_validation/ci/utils.py
@@ -1,6 +1,13 @@
 import os
+import yaml
 
 from catalog_validation.items.utils import DEVELOPMENT_DIR
+
+
+def get_app_version(app_path: str) -> str:
+    # This assumes that file exists and version is specified and is good
+    with open(os.path.join(app_path, 'Chart.yaml'), 'r') as f:
+        return yaml.safe_load(f.read())['version']
 
 
 def get_ci_development_directory(catalog_path: str) -> str:
--- catalog_validation/ci/validate.py
+++ catalog_validation/ci/validate.py
@@ -1,8 +1,9 @@
 import os
-import yaml
 
 from catalog_validation.exceptions import ValidationErrors
 from catalog_validation.validation import validate_catalog_item_version, validate_chart_version
+
+from .utils import get_app_version
 
 
 def validate_dev_directory_structure(catalog_path: str) -> None:
@@ -10,14 +11,32 @@ def validate_dev_directory_structure(catalog_path: str) -> None:
     verrors.check()
 
 
-def validate_app(app_dir_path: str) -> None:
+def validate_train(catalog_path: str, train_path: str, schema: str) -> None:
+    verrors = ValidationErrors()
+    train_name = os.path.basename(train_path)
+    for app_name in filter(os.path.isdir, os.listdir(train_path)):
+        app_path = os.path.join(train_path, app_name)
+        try:
+            validate_app(app_path, f'{schema}.{app_name}')
+        except ValidationErrors as ve:
+            verrors.extend(ve)
+        else:
+            train_app_path = os.path.join(catalog_path, train_name, app_name)
+            if not os.path.exists(train_app_path):
+                # The application is new and we are good
+                continue
+
+            if get_app_version(app_path) in os.listdir(train_path):
+                verrors.add(
+                    f'{schema}.{app_name}.version',
+                    'Version must be bumped as app has been changed but version has not been updated'
+                )
+
+
+def validate_app(app_dir_path: str, schema: str) -> None:
     app_name = os.path.basename(app_dir_path)
-    error_schema = f'dev.{app_name}'
     chart_version_path = os.path.join(app_dir_path, 'Chart.yaml')
-    verrors = validate_chart_version(ValidationErrors(), chart_version_path, error_schema, app_name)
+    verrors = validate_chart_version(ValidationErrors(), chart_version_path, schema, app_name)
     verrors.check()
 
-    with open(chart_version_path, 'r') as f:
-        chart_config = yaml.safe_load(f.read())
-
-    validate_catalog_item_version(app_dir_path, error_schema, chart_config['version'])
+    validate_catalog_item_version(app_dir_path, schema, get_app_version(app_dir_path))
