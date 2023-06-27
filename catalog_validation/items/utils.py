2f926e6466eacd432a0d880d64b9e4dac88f4591
Waqar Ahmed <waqarahmedjoyia@live.com>
2023-06-27 18:44:16 +0500

    Use subprocess instead of python git module to get last updated date

--- catalog_validation/items/utils.py
+++ catalog_validation/items/utils.py
@@ -1,6 +1,6 @@
 import contextlib
-import git
 import os
+import subprocess
 
 from datetime import datetime
 from typing import Optional
@@ -207,13 +207,14 @@ def get_catalog_json_schema() -> dict:
 def get_last_updated_date(repo_path: str, folder_path: str) -> Optional[str]:
     with contextlib.suppress(Exception):
         # We don't want to fail querying items if for whatever reason this fails
-        repo = git.Repo(repo_path)
-        latest_commit = next(repo.iter_commits(paths=folder_path, max_count=1), None)
-        if latest_commit:
-            timestamp = datetime.fromtimestamp(latest_commit.committed_date)
+        output = subprocess.check_output(
+            ['git', 'log', '-n', '1', '--pretty=format:%ct', f'{folder_path}'],
+            cwd=repo_path,
+            stderr=subprocess.DEVNULL
+        )
+        if output:
+            timestamp = datetime.fromtimestamp(int(output))
             return timestamp.strftime('%Y-%m-%d %H:%M:%S')
-        else:
-            return None
 
 
 def valid_train(train_name: str, train_location: str) -> bool:
