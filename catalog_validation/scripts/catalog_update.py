6049dd52ab32a9fd6079e6cc5fccd36a1afcd840
Waqar Ahmed <waqarahmedjoyia@live.com>
2023-11-07 13:52:53 +0500

    Refactor get trains implementation to retrieve versions data
    (cherry picked from commit 5dc4b184f76adefd7bf19b3113e9509dc79b9cef)

--- catalog_validation/scripts/catalog_update.py
+++ catalog_validation/scripts/catalog_update.py
@@ -4,6 +4,7 @@ import contextlib
 import json
 import os
 import shutil
+import typing
 
 from jsonschema import validate as json_schema_validate, ValidationError as JsonValidationError
 
@@ -18,13 +19,26 @@ from catalog_validation.utils import CACHED_CATALOG_FILE_NAME
 from collections import defaultdict
 
 
-def get_trains(location: str) -> dict:
+def get_trains(location: str) -> typing.Tuple[dict, dict]:
     preferred_trains: list = []
-
     trains_to_traverse = retrieve_train_names(location)
-    items = get_items_in_trains(trains_to_traverse, location)
+    catalog_data = {}
+    versions_data = {}
+    for train_name, train_data in retrieve_trains_data(
+        get_items_in_trains(trains_to_traverse, location), location, preferred_trains, trains_to_traverse
+    )[0].items():
+        catalog_data[train_name] = {}
+        versions_data[train_name] = {}
+        for app_name, app_data in train_data.items():
+            catalog_data[train_name][app_name] = {}
+            versions_data[train_name][app_name] = {}
+            for k, v in app_data.items():
+                if k == 'versions':
+                    versions_data[train_name][app_name][k] = v
+                else:
+                    catalog_data[train_name][app_name][k] = v
 
-    return retrieve_trains_data(items, location, preferred_trains, trains_to_traverse)[0]
+    return catalog_data, versions_data
 
 
 def validate_train_data(train_data):
