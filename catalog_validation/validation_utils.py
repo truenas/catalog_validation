2c010cf6445f6f94b8ba033972941542ced13f95
Waqar Ahmed <waqarahmedjoyia@live.com>
2023-11-12 01:22:15 +0500

    Validate the format of specified min/max scale versions is accurate

--- catalog_validation/validation_utils.py
+++ catalog_validation/validation_utils.py
@@ -1,10 +1,36 @@
 import os
 import yaml
 
+from middlewared.plugins.update_.utils import can_update
 from semantic_version import Version
 from typing import Optional
 
 from .exceptions import ValidationErrors
+from .utils import validate_key_value_types, RE_SCALE_VERSION
+
+
+def validate_min_max_version_values(annotations_dict, verrors, schema):
+    validate_key_value_types(
+        annotations_dict, (('min_scale_version', str, False), ('max_scale_version', str, False)), verrors, schema
+    )
+
+    if verrors:
+        # No point in proceeding further
+        return
+
+    for version in filter(lambda v: v in annotations_dict, ['min_scale_version', 'max_scale_version']):
+        if not RE_SCALE_VERSION.match(annotations_dict[version]):
+            verrors.add(
+                f'{schema}.{version}',
+                f'Format of provided {version} value is not correct'
+            )
+
+    if (
+        not verrors and all(version in annotations_dict for version in ['min_scale_version', 'max_scale_version']) and
+        annotations_dict['min_scale_version'] != annotations_dict['max_scale_version'] and
+        not can_update(annotations_dict['min_scale_version'], annotations_dict['max_scale_version'])
+    ):
+        verrors.add(schema, 'Provided min_scale_version is greater than provided max_scale_version')
 
 
 def validate_chart_version(
@@ -25,6 +51,8 @@ def validate_chart_version(
 
                     if not isinstance(chart_config.get('annotations', {}), dict):
                         verrors.add(f'{schema}.annotations', 'Annotations must be a dictionary')
+                    elif chart_config.get('annotations'):
+                        validate_min_max_version_values(chart_config['annotations'], verrors, schema)
 
                     if not isinstance(chart_config.get('sources', []), list):
                         verrors.add(f'{schema}.sources', 'Sources must be a list')
