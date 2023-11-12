00f41986aa95679af16b9d55bc230f9e60597985
Waqar Ahmed <waqarahmedjoyia@live.com>
2023-11-13 04:02:18 +0500

    Properly specify start/end in scale version regex

--- catalog_validation/utils.py
+++ catalog_validation/utils.py
@@ -44,7 +44,7 @@ METADATA_JSON_SCHEMA = {
         },
     },
 }
-RE_SCALE_VERSION = re.compile(r'(\d{2}\.\d{2}(?:\.\d)*(?:-?(?:RC|BETA)\.?\d?)?)')  # 24.04 / 24.04.1 / 24.04-RC.1
+RE_SCALE_VERSION = re.compile(r'^(\d{2}\.\d{2}(?:\.\d)*(?:-?(?:RC|BETA)\.?\d?)?)$')  # 24.04 / 24.04.1 / 24.04-RC.1
 RE_VERSION_PATTERN = re.compile(r'(\d{2}\.\d{2}(?:\.\d)*)')  # We are only interested in XX.XX here
 VALID_TRAIN_REGEX = re.compile(r'^\w+[\w.-]*$')
 VERSION_VALIDATION_SCHEMA = {
